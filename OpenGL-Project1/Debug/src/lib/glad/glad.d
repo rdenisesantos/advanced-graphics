9ec9a85f75096018d0e9dc966fa2228aa4bed6c3
Denise <denisesantos.8c@gmail.com>
2019-02-23 13:06:01 -0800

    First OpenGl Project - RGB Interpolation

---
+++ OpenGL-Project1/Debug/src/lib/glad/glad.d
@@ -0,0 +1,6 @@
+src/lib/glad/glad.o: ../src/lib/glad/glad.c ../src/lib/glad/glad.h \
+ ../src/lib/glad/khrplatform.h
+
+../src/lib/glad/glad.h:
+
+../src/lib/glad/khrplatform.h:
---
+++ OpenGL-Project1/Debug/src/main.d
@@ -0,0 +1,6 @@
+src/main.o: ../src/main.cpp ../src/./lib/glad/glad.h \
+ ../src/./lib/glad/khrplatform.h
+
+../src/./lib/glad/glad.h:
+
+../src/./lib/glad/khrplatform.h:
