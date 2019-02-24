48bf41307afd988c59a157f0e0ffd63cc55c5a5b
Denise <denisesantos.8c@gmail.com>
2019-02-24 00:02:12 -0800

    Renders a rectangle and attaches a texture

---
+++ OpenGL-Project2/Debug/src/main.d
@@ -0,0 +1,8 @@
+src/main.o: ../src/main.cpp ../src/./lib/glad/glad.h \
+ ../src/./lib/glad/khrplatform.h ../src/./lib/stb_image.h
+
+../src/./lib/glad/glad.h:
+
+../src/./lib/glad/khrplatform.h:
+
+../src/./lib/stb_image.h:
