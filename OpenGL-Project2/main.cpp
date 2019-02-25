2c5d8f86685ebbf9aeeefbbcd2e0c2151acd018c
rdenisesantos <26937214+rdenisesantos@users.noreply.github.com>
2019-02-25 00:37:58 -0800

    Changed background color
    Changed background to black

--- OpenGL-Project2/main.cpp
+++ OpenGL-Project2/main.cpp
@@ -33,26 +33,26 @@ float lastFrame = 0.0f;
 
 int main()
 {
-	// glfw: initialize and configure
-	// ------------------------------
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
 #ifdef __APPLE__
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
 #endif
-
-	// glfw window creation
-	// --------------------
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "COMP 565 - OpenGL - Homework 2 ", NULL, NULL);
-	if (window == NULL)
-	{
-    	std::cout << "Failed to create GLFW window" << std::endl;
-    	glfwTerminate();
-    	return -1;
-	}
+    
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "COMP 565 - OpenGL - Homework 2 ", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     glfwSetCursorPosCallback(window, mouse_callback);
@@ -60,133 +60,132 @@ int main()
     
     // tell GLFW to capture our mouse
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-
-	// glad: load all OpenGL function pointers
-	// ---------------------------------------
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-    	std::cout << "Failed to initialize GLAD" << std::endl;
-    	return -1;
-	}
-
-	// configure global opengl state
-	// -----------------------------
-	glEnable(GL_DEPTH_TEST);
+    
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    
+    // configure global opengl state
+    // -----------------------------
+    glEnable(GL_DEPTH_TEST);
     
     // build and compile our shader program
     // ------------------------------------
     Shader ourShader("shader.vs", "shader.fs");
-
-	// set up vertex data (and buffer(s)) and configure vertex attributes
-	// ------------------------------------------------------------------
- float vertices[] = {
-    	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-     	0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-     	0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-     	0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-    	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-    	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-
-    	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-     	0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-     	0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-     	0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-    	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-    	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-
-    	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-    	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-    	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-    	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-    	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-    	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-     	0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-     	0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-     	0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-     	0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-     	0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-     	0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-    	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-     	0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-     	0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-     	0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-    	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-    	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-
-    	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-     	0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-     	0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-     	0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-    	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-    	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-	};
-
- // world space positions of our cubes
- glm::vec3 cubePositions[385];
-// float starting_x=-5.0f, starting_y=-4.0f, starting_z=-20.0f;
-float starting_x=-3.8f, starting_y=-8.8f, starting_z=-4.0f;
- int index = 0;
- float n = 10.0f;
- for(float y=starting_y;y<10.0f;y=y+1.0f){
-     	for(float z=starting_z; z<(starting_z+n);z=z+1.0f){
-             	for(float x=starting_x; x<(starting_x+n);x=x+1.0f){
-                     	cubePositions[index]=glm::vec3(x,y,z);
-                     	index++;
-             	}
-     	}
-     	starting_x=starting_x+0.5;
-     	starting_z=starting_z+0.5;
-     	n--;
- }
-// cubePositions[384]=glm::vec3(starting_x-2.5f,starting_y+10.0f,starting_z-2.5f);
+    
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    float vertices[] = {
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+        
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+    };
+    
+    // world space positions of our cubes
+    glm::vec3 cubePositions[385];
+    // float starting_x=-5.0f, starting_y=-4.0f, starting_z=-20.0f;
+    float starting_x=-3.8f, starting_y=-8.8f, starting_z=-4.0f;
+    int index = 0;
+    float n = 10.0f;
+    for(float y=starting_y;y<10.0f;y=y+1.0f){
+        for(float z=starting_z; z<(starting_z+n);z=z+1.0f){
+            for(float x=starting_x; x<(starting_x+n);x=x+1.0f){
+                cubePositions[index]=glm::vec3(x,y,z);
+                index++;
+            }
+        }
+        starting_x=starting_x+0.5;
+        starting_z=starting_z+0.5;
+        n--;
+    }
     cubePositions[384]=glm::vec3(0.6f,0.6f,0.6f);
-
-	unsigned int VBO, VAO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-
-	glBindVertexArray(VAO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	// position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	// color attribute
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
-
-	// load image, create texture and generate mipmaps
-	int width, height, nrChannels;
-	unsigned char *data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
-	unsigned int texture; // load and create a texture
-	glGenTextures(1, &texture);
-	glBindTexture(GL_TEXTURE_2D, texture); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
-	if (data)
-	{
-    	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-    	glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else
-	{
-    	std::cout << "Failed to load texture" << std::endl;
-	}
-	// set the texture wrapping parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);   	// set texture wrapping to GL_REPEAT (default wrapping method)
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	// set texture filtering parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	stbi_image_free(data);
-
+    
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    
+    glBindVertexArray(VAO);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    
+    // load image, create texture and generate mipmaps
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+    unsigned int texture; // load and create a texture
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);       // set texture wrapping to GL_REPEAT (default wrapping method)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    stbi_image_free(data);
+    
     ourShader.use();
-	// render loop
-	// -----------
-	while (!glfwWindowShouldClose(window))
-	{
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
         // per-frame time logic
         // --------------------
         float currentFrame = glfwGetTime();
@@ -196,32 +195,32 @@ float starting_x=-3.8f, starting_y=-8.8f, starting_z=-4.0f;
         // input
         // -----
         processInput(window);
-
-    	// render
-    	// ------
-    	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-    	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // also clear the depth buffer now!
-
-    	// bind Texture
-    	glBindTexture(GL_TEXTURE_2D, texture);
-
+        
+        // render
+        // ------
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // also clear the depth buffer now!
+        
+        // bind Texture
+        glBindTexture(GL_TEXTURE_2D, texture);
+        
         // activate shader
         ourShader.use();
-
+        
         glm::mat4 view          = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
         glm::mat4 projection    = glm::mat4(1.0f);
         
-    	// render boxes
-    	glBindVertexArray(VAO);
-    	for(unsigned int i=0;i < 384; i++)
-    	{
-        	// calculate the model matrix for each object and pass it to shader before drawing
-        	glm::mat4 model = glm::mat4(1.0f);
-        	model = glm::translate(model, cubePositions[i]);
+        // render boxes
+        glBindVertexArray(VAO);
+        for(unsigned int i=0;i < 384; i++)
+        {
+            // calculate the model matrix for each object and pass it to shader before drawing
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
             ourShader.setMat4("model",model);
-        	glDrawArrays(GL_TRIANGLES, 0, 36);
-    	}
-    	// highest cube
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+        // highest cube
         glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
         model = glm::translate(model, cubePositions[384]);
         model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
@@ -237,30 +236,30 @@ float starting_x=-3.8f, starting_y=-8.8f, starting_z=-4.0f;
         
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 36);
-
-    	// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-    	// -------------------------------------------------------------------------------
-    	glfwSwapBuffers(window);
-    	glfwPollEvents();
-	}
-
-	// optional: de-allocate all resources once they've outlived their purpose:
-	// ------------------------------------------------------------------------
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-
-	// glfw: terminate, clearing all previously allocated GLFW resources.
-	// ------------------------------------------------------------------
-	glfwTerminate();
-	return 0;
+        
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
 }
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow *window)
 {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-    	glfwSetWindowShouldClose(window, true);
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
         camera.ProcessKeyboard(FORWARD, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
@@ -279,9 +278,9 @@ void processInput(GLFWwindow *window)
 // ---------------------------------------------------------------------------------------------
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-	// make sure the viewport matches the new window dimensions; note that width and
-	// height will be significantly larger than specified on retina displays.
-	glViewport(0, 0, width, height);
+    // make sure the viewport matches the new window dimensions; note that width and
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
 }
 
 // glfw: whenever the mouse moves, this callback is called
